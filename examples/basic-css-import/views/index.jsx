3915a6d34a16f5b9cb784b859c039b9cb5711acb
e0 <shan.huang@me.com>
2019-11-19 00:46:35 +0100

    Add failing layout css import example

--- examples/basic-css-import/views/index.jsx
+++ examples/basic-css-import/views/index.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import '../styles/index.css';
+const Index = ({user}) => {
+  const [message, setMessage] = React.useState("waiting...");
 
-const Index = ({ user }) => {
-  const [message, setMessage] = React.useState('waiting...');
-
-  const onClick = () => setMessage('This is a react-ssr!');
+  const onClick = () => setMessage("This is a react-ssr!");
 
   return (
     <React.Fragment>
